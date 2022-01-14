a0a01228795c0c1293f3f453c5a29a2a458c2fa8
Yana Ivanova <yaxo4ka1988@gmail.com>
2022-01-14 20:16:00 +0200

    changed 3rd one

--- js/task-03.js
+++ js/task-03.js
@@ -1,30 +1,45 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
 
-const gallery = document.querySelector(".gallery");
-const createGallery  = function (items = []) { 
-  let htmlImg = "";
-  for (let index = 0; index < items.length; index+=1){
-    htmlImg +=
-    "<li><img src = " +
-    items[index].url +
-    "alt = " +
-    items[index].alt +
-    "></li>";
-  }
-  return htmlImg;
+const galleryEl = document.querySelector(`.gallery`);
+
+const makeGalleryCard = ({ url, alt } = {}) => {
+  return `<li class="js-item"><img class="js-image" src="${url}"  alt="${alt}" ></li>`;
 };
-console.log(createGallery(images));
-gallery.insertAdjacentHTML("beforeend", createGallery(images));
+
+const galleryLiElements = images
+  .map((pic) => {
+    return makeGalleryCard(pic);
+  })
+  .join(``);
+console.log(galleryLiElements);
+
+galleryEl.insertAdjacentHTML(`beforeend`, galleryLiElements);
+
+// const gallery = document.querySelector(".gallery");
+// const createGallery  = function (items = []) {
+//   let htmlImg = "";
+//   for (let index = 0; index < items.length; index+=1){
+//     htmlImg +=
+//     "<li><img src = " +
+//     items[index].url +
+//     "alt = " +
+//     items[index].alt +
+//     "></li>";
+//   }
+//   return htmlImg;
+// };
+// console.log(createGallery(images));
+// gallery.insertAdjacentHTML("beforeend", createGallery(images));
